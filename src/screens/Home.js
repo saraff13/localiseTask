de9a5f59cd75cb25eab4d9d7a9ce3ba0e01d8201
samarasunny22 <psamarasimha.reddy@xebia.com>
2021-09-16 11:23:47 +0530

    Partial conversion of classes to Hooks

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import {connect} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {getTranslation} from '../store/actions/translateAction';
 import {
   addLanguage,
@@ -17,128 +17,33 @@ const API = {
   hn: 'https://mocki.io/v1/d534f590-0456-44d3-a0e8-f5529717e053',
 };
 
-class Home extends Component {
-  state = {
-    loadedData: '',
+const Home = () => {
+  const [loadedData, setLoadedData] = useState('');
+  const dispatch = useDispatch();
+  const data = useSelector(state => state.translateReducer.data);
+
+  useEffect(() => {
+    if (data) setLoadedData(data[0]);
+  });
+
+  const changeLanguage = (api, id) => {
+    console.log('Language id =>', id);
+    dispatch(getTranslation(api));
   };
-  componentDidMount() {
-    filterActiveLanguage()
-      .then(language => {
-        if (language) {
-          console.log('filter success => ', language);
-          this.setState({loadedData: language[0]});
-        } else {
-          this.props.getTranslation(API.en);
-        }
-      })
-      .catch(error => console.log('filter error => ', error));
-  }
-  componentDidUpdate(prevProps) {
-    if (prevProps.data != this.props.data) {
-      const newData = this.props.data && this.props.data[0];
-      // console.log(newData);
-      const newLanguage = {
-        id: newData.id,
-        name: newData.name,
-        how: newData.how,
-        boiledEgg: newData.boiledEgg,
-        softBoiledEgg: newData.softBoiledEgg,
-        choice: newData.choice,
-        active: 'true',
-      };
-      if (prevProps.data) {
-        updateStatus(prevProps.data[0].id, 'false')
-          .then(() =>
-            console.log('successfully disabled => ', prevProps.data[0].id),
-          )
-          .catch(error => console.log('disabled error => ', error));
-      }
-      // if (prevProps.data) {
-      //   // console.log('Previous Props => ', prevProps.data);
-      //   deleteLanguage(prevProps.data[0].id)
-      //     .then(() => console.log('successfully deleted'))
-      //     .catch(error => console.log('deletion error => ', error));
-      // }
-      // console.log(newLanguage);
-      addLanguage(newLanguage)
-        .then(() => console.log('successfully added'))
-        .catch(error => console.log('realm addition error => ', error));
-      queryAllLanguages()
-        .then(languages => {
-          console.log('query => ', languages);
-        })
-        .catch(error => console.log('query realm error => ', error));
-      this.setState({loadedData: this.props.data[0]});
-      return true;
-    }
-    return false;
-  }
-  changeLanguage = (api, id) => {
-    // console.log(this.state.loadedData);
-    if (this.state.loadedData && this.state.loadedData.id === id) {
-      return;
-    }
-    getLanguageData(id)
-      .then(language => {
-        if (language) {
-          if (this.state.loadedData) {
-            updateStatus(this.state.loadedData.id, 'false')
-              .then(() =>
-                console.log(
-                  'successfully disabled => ',
-                  this.state.loadedData.id,
-                ),
-              )
-              .catch(error => console.log('disabled error => ', error));
-          }
-          updateStatus(id, 'true')
-            .then(value => {
-              console.log('successfully enabled => ', id);
-              this.setState({loadedData: value});
-            })
-            .catch(error => console.log('enabled error => ', error));
-          // console.log('get language data => ', language);
-        } else {
-          this.props.getTranslation(api);
-        }
-      })
-      .catch(error => {
-        console.log('get language data error => ', error);
-      });
-  };
-  render() {
-    const {data} = this.props;
-    const {loadedData} = this.state;
-    return (
-      <SafeAreaView style={[styles.main]}>
-        <View style={[styles.languageBox]}>
-          <Button
-            title={'English'}
-            onPress={() => this.changeLanguage(API.en, 1)}
-          />
-          <Button
-            title={'हिंदी'}
-            onPress={() => this.changeLanguage(API.hn, 2)}
-          />
-          <Button
-            title={'italiana'}
-            onPress={() => this.changeLanguage(API.it, 3)}
-          />
-        </View>
 
-        <Text style={[styles.text]}>
-          My Lang: {loadedData && loadedData.how}
-        </Text>
-      </SafeAreaView>
-    );
-  }
-}
+  return (
+    <SafeAreaView style={[styles.main]}>
+      <View style={[styles.languageBox]}>
+        <Button title={'English'} onPress={() => changeLanguage(API.en, 1)} />
+        <Button title={'हिंदी'} onPress={() => changeLanguage(API.hn, 2)} />
+        <Button title={'italiana'} onPress={() => changeLanguage(API.it, 3)} />
+      </View>
 
-const mapStateToProps = state => ({
-  data: state.translateReducer.data,
-});
-
-export default connect(mapStateToProps, {getTranslation})(Home);
+      <Text style={[styles.text]}>My Lang: {loadedData && loadedData.how}</Text>
+    </SafeAreaView>
+  );
+};
+export default Home;
 
 const styles = StyleSheet.create({
   main: {
