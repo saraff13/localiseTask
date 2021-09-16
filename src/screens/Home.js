09acd2f6d19ea99713ab39fd527ebdef7d1d4030
samarasunny22 <psamarasimha.reddy@xebia.com>
2021-09-16 12:35:46 +0530

    Full conversion of Class to Functions

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,9 +12,9 @@ import {
 } from '../databases/schema';
 
 const API = {
-  en: 'https://mocki.io/v1/4b81fc7f-6886-4ca0-8a82-ecdeed8d244e',
-  it: 'https://mocki.io/v1/c4a8e3c6-4df5-4924-b82c-a071bcba52a1',
-  hn: 'https://mocki.io/v1/d534f590-0456-44d3-a0e8-f5529717e053',
+  en: 'https://mocki.io/v1/0c9cdfaa-888c-4306-a0a4-09cf4027cb7c',
+  it: 'https://mocki.io/v1/f3ef7b2a-d151-4ed0-8dd0-1a12c765486e',
+  hn: 'https://mocki.io/v1/d39cab01-d2bb-4667-86cc-af54d4afe58f',
 };
 
 const Home = () => {
@@ -23,12 +23,77 @@ const Home = () => {
   const data = useSelector(state => state.translateReducer.data);
 
   useEffect(() => {
-    if (data) setLoadedData(data[0]);
-  });
+    filterActiveLanguage()
+      .then(language => {
+        if (language.length) {
+          console.log('filter success => ', language);
+          setLoadedData(language[0]);
+        } else {
+          dispatch(getTranslation(API.en));
+        }
+      })
+      .catch(error => console.log('filter error => ', error));
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      const newLanguage = {
+        id: data.id,
+        name: data.name,
+        how: data.how,
+        boiledEgg: data.boiledEgg,
+        softBoiledEgg: data.softBoiledEgg,
+        choice: data.choice,
+        active: 'true',
+      };
+      addLanguage(newLanguage)
+        .then(() => console.log('successfully added'))
+        .catch(error => console.log('realm addition error => ', error));
+      queryAllLanguages()
+        .then(languages => {
+          console.log('query => ', languages);
+        })
+        .catch(error => console.log('query realm error => ', error));
+      setLoadedData(data);
+    }
+  }, [data]);
 
   const changeLanguage = (api, id) => {
-    console.log('Language id =>', id);
-    dispatch(getTranslation(api));
+    // console.log('Language id =>', id);
+    if (loadedData && loadedData.id === id) {
+      return;
+    }
+
+    getLanguageData(id)
+      .then(language => {
+        if (language) {
+          if (loadedData) {
+            console.log('Loaded Data=>', loadedData);
+            updateStatus(loadedData.id, 'false')
+              .then(() =>
+                console.log('successfully disabled => ', loadedData.id),
+              )
+              .catch(error => console.log('disabled error => ', error));
+          }
+          updateStatus(id, 'true')
+            .then(value => {
+              console.log('successfully enabled => ', id);
+              setLoadedData(value);
+            })
+            .catch(error => console.log('enabled error => ', error));
+          // console.log('get language data => ', language);
+          console.log('Langauge Found');
+          setLoadedData(language);
+        } else {
+          updateStatus(loadedData.id, 'false')
+            .then(() => console.log('successfully disabled => ', loadedData.id))
+            .catch(error => console.log('disabled error => ', error));
+          dispatch(getTranslation(api));
+        }
+      })
+      .catch(error => {
+        console.log('Get Language error=>', error);
+      });
   };
 
   return (
